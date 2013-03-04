0089f9d67fd47eb4fb1c61cce2f15a3a6a8db85a
Zoran Merki <zoranmerki@gmail.com>
2013-03-04 20:42:15 +0100

    pocetna verzija projekta

---
+++ src/javaapplication18/JavaApplication18.java
@@ -0,0 +1,16 @@
+package javaapplication18;
+
+/**
+ *
+ * @author Zoran
+ */
+public class JavaApplication18 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Proba");
+    }
+}
